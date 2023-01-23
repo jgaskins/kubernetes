27b6ae243d2faf37d3d54d2504f711e7836ab612
Jamie Gaskins <jgaskins@hey.com>
2023-01-23 00:54:12 -0500

    Handle multiple CRDs in a single import

--- src/import_crd.cr
+++ src/import_crd.cr
@@ -5,30 +5,32 @@ require "./serializable"
 require "./crd"
 
 begin
-  crd = Kubernetes::CRD.from_yaml(File.read(ARGV[0]))
+  File.read(ARGV[0]).split(/^---$/).each do |yaml|
+    crd = Kubernetes::CRD.from_yaml(yaml)
 
-  version = crd.spec.versions.find { |v| v.storage }.not_nil!
-  properties = version.schema.open_api_v3_schema.properties.spec.properties
+    version = crd.spec.versions.find { |v| v.storage }.not_nil!
+    properties = version.schema.open_api_v3_schema.properties.spec.properties
 
-  code = <<-CRYSTAL
-  struct #{crd.spec.names.kind}
-    include Kubernetes::Serializable
+    code = <<-CRYSTAL
+    struct #{crd.spec.names.kind}
+      include Kubernetes::Serializable
 
-    #{properties.to_crystal}
+      #{properties.to_crystal}
+    end
+
+    Kubernetes.define_resource(
+      name: #{crd.spec.names.plural.inspect},
+      group: #{crd.spec.group.inspect},
+      type: Kubernetes::Resource(#{crd.spec.names.kind}),
+      kind: "#{crd.spec.names.kind}",
+      version: #{version.name.inspect},
+      prefix: "apis",
+      singular_name: #{crd.spec.names.singular.inspect},
+    )
+    CRYSTAL
+
+    puts code
   end
-
-  Kubernetes.define_resource(
-    name: #{crd.spec.names.plural.inspect},
-    group: #{crd.spec.group.inspect},
-    type: Kubernetes::Resource(#{crd.spec.names.kind}),
-    kind: "#{crd.spec.names.kind}",
-    version: #{version.name.inspect},
-    prefix: "apis",
-    singular_name: #{crd.spec.names.singular.inspect},
-  )
-  CRYSTAL
-
-  puts code
 rescue ex
   STDERR.puts ex
   STDERR.puts ex.pretty_inspect
